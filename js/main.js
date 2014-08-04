3ebb20c9550b3aceb6fcf87d211eeb16465e3d7e
Simkovich <dand.was@gmail.com>
2014-08-04 17:36:54 +0300

    catalog done

--- js/main.js
+++ js/main.js
@@ -10,4 +10,50 @@ $(function(){
       }
   );
   /* /catalog selection hover*/
+
+  /* add-remove vin table rows */
+  function countRowNumber() {
+    var count = 0;
+    var rows = $(".form-table").find("tr");
+    rows.each(function(){
+      $(this).children("td:eq(0)").text(count++);
+    });
+  }
+
+  $(".add_row").on("click",function(ev){
+    ev.preventDefault();
+    $(this).parent().parent().parent().append(
+       "<tr>" +
+          "<td>&nbsp;</td>"+
+          "<td><input class=\"form-control mini\" type=\"text\" value=\"\"/></td>" +
+          "<td><input class=\"form-control mini\" type=\"text\" value=\"\"/></td>" +
+          "<td><input class=\"form-control mini\" type=\"number\" value=\"1\"></td>" +
+          "<td><input class=\"form-control mini\" type=\"text\"/></td>" +
+          "<td class=\"text-center\"><a class=\"rem_row\" href=\"#\"><img src=\"images/icons/rem.png\" alt=\"\"/></a></td>" +
+        "</tr>"
+    );
+    countRowNumber();
+  });
+
+  $(".rem_row").on("click",function(ev){
+    ev.preventDefault();
+    $(this).parent().parent().remove();
+    countRowNumber();
+  });
+  /* /add-remove vin table rows */
+
+  /* show-hide submenu in catalog filter*/
+  $(".ctlg-sidebar_cat_li").on("click", function(ev){
+    ev.preventDefault();
+    $(this).parent().find(".active").removeClass("active");
+    $(this).addClass("active");
+    $(this).find("ul").addClass("open").fadeIn();
+  });
+
+  $(document).on("click", ".subcat_li", function(ev) {
+    ev.preventDefault();
+    $(this).parent("ul").find(".active").removeClass("active");
+    $(this).addClass("active");
+  });
+  /* /show-hide submenu in catalog filter*/
 });
